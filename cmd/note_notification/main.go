a4b30e9090b6c3d1c81a2bcf31b76c67a80b6db0
DanielChachagua <danielmchachagua@gmail.com>
2025-07-04 01:24:01 -0300

    add aip calendar

--- cmd/note_notification/main.go
+++ cmd/note_notification/main.go
@@ -1,21 +1,101 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"note_notifications/cmd/note_notification/commands"
 	"note_notifications/cmd/note_notification/jobs"
 	"note_notifications/internal/database"
 	"note_notifications/internal/dependencies"
-	"note_notifications/internal/schemas"
+	// "note_notifications/internal/schemas"
 	"os"
-	"runtime"
+	// "runtime"
+	"time"
 
 	// "github.com/gen2brain/beeep"
 	"github.com/joho/godotenv"
+	"golang.org/x/oauth2/google"
+	calendar "google.golang.org/api/calendar/v3"
 	// "github.com/robfig/cron/v3"
+	// "github.com/gen2brain/dlgs"
 )
 
 func main() {
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("No se pudo leer el archivo credentials.json: %v", err)
+	}
+
+	config, err := google.ConfigFromJSON(b, calendar.CalendarReadonlyScope)
+	if err != nil {
+		log.Fatalf("Error al parsear config: %v", err)
+	}
+	client := jobs.GetClient(config)
+
+	srv, err := calendar.New(client)
+	if err != nil {
+		log.Fatalf("No se pudo crear el cliente de Calendar: %v", err)
+	}
+
+	t := time.Now().Format(time.RFC3339)
+	events, err := srv.Events.List("primary").ShowDeleted(false).
+		SingleEvents(true).TimeMin(t).MaxResults(10).OrderBy("startTime").Do()
+	if err != nil {
+		log.Fatalf("No se pudieron obtener los eventos: %v", err)
+	}
+
+	fmt.Println("Próximos eventos:")
+	if len(events.Items) == 0 {
+		fmt.Println("No hay eventos próximos.")
+	} else {
+		for _, item := range events.Items {
+			date := item.Start.DateTime
+			if date == "" {
+				date = item.Start.Date
+			}
+			fmt.Printf("%v - %s\n", date, item.Summary)
+		}
+	}
+	// confirmed, err := dlgs.Question("Apagar sistema", "¿Querés apagar la computadora?", true)
+	// dlgs.Warning("Apagando sistema...", "Apagando sistema2...")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// item, _, err := dlgs.List("List", "Select item from list:", []string{"Bug", "New Feature", "Improvement"})
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// passwd, _, err := dlgs.Password("Password", "Enter your API key:")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// val, bol , err := dlgs.Entry("fecha", "Enter your API key:", "02-01-2006")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(val, bol)
+
+	// if item == "Bug" {
+	// 	fmt.Println("Apagando sistema...")
+	// 	panic("Apagando sistema...")
+	// 	// Aquí ejecutás tu comando de apagado
+	// }
+
+	// if passwd == "1234" {
+	// 	fmt.Println("Apagando sistema...")
+	// 	panic("Apagando sistema...")
+	// 	// Aquí ejecutás tu comando de apagado
+	// }
+
+	// if confirmed {
+	// 	fmt.Println("Apagando sistema...")
+	// 	panic("Apagando sistema...")
+	// 	// Aquí ejecutás tu comando de apagado
+	// } else {
+	// 	fmt.Println("Cancelado")
+	// }
+
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error al cargar el archivo .env")
 	}
@@ -26,19 +106,19 @@ func main() {
 	}
 	defer database.CloseDB(db)
 
-	noteChan := make(chan []schemas.NoteResponse)
+	// noteChan := make(chan []schemas.NoteResponse)
 
-	runtime.LockOSThread()
+	// runtime.LockOSThread()
 
 	deps := dependencies.NewContainer(db)
 
-	go func() {
-		for notes := range noteChan {
-			jobs.ShowAllNotesInOneWindow(&notes) // ✅ SE EJECUTA EN EL MAIN THREAD
-		}
-	}()
+	// go func() {
+	// 	for notes := range noteChan {
+	// 		jobs.ShowAllNotesInOneWindow(&notes) // ✅ SE EJECUTA EN EL MAIN THREAD
+	// 	}
+	// }()
 
-	jobs.InitNotifications(deps, noteChan)
+	// jobs.InitNotifications(deps, noteChan)
 
 	commands.Execute(deps)
 
--- go.mod
+++ go.mod
@@ -5,30 +5,67 @@ go 1.24.1
 require github.com/olekukonko/tablewriter v1.0.7
 
 require (
+	cloud.google.com/go/auth v0.16.2 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
+	cloud.google.com/go/compute/metadata v0.7.0 // indirect
+	git.sr.ht/~jackmordaunt/go-toast v1.1.2 // indirect
+	github.com/esiqveland/notify v0.13.3 // indirect
 	github.com/fatih/color v1.18.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/google/s2a-go v0.1.9 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
+	github.com/googleapis/gax-go/v2 v2.14.2 // indirect
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jackmordaunt/icns/v3 v3.0.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/leaanthony/slicer v1.6.0 // indirect
+	github.com/leaanthony/u v1.1.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/olekukonko/errors v1.1.0 // indirect
 	github.com/olekukonko/ll v0.0.9 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sergeymakinen/go-bmp v1.0.0 // indirect
+	github.com/sergeymakinen/go-ico v1.0.0-beta.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.61.0 // indirect
+	go.opentelemetry.io/otel v1.36.0 // indirect
+	go.opentelemetry.io/otel/metric v1.36.0 // indirect
+	go.opentelemetry.io/otel/trace v1.36.0 // indirect
+	golang.org/x/crypto v0.39.0 // indirect
+	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/oauth2 v0.30.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
+	google.golang.org/api v0.240.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250603155806-513f23925822 // indirect
+	google.golang.org/grpc v1.73.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
 
 require (
+	github.com/gen2brain/beeep v0.11.1
+	github.com/gen2brain/dlgs v0.0.0-20220603100644-40c77870fa8d
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/joho/godotenv v1.5.1
 	github.com/spf13/cobra v1.9.1
+	github.com/wailsapp/wails/v2 v2.10.1
 	github.com/webview/webview_go v0.0.0-20240831120633-6173450d4dd6
 	golang.org/x/sys v0.33.0 // indirect
 	gorm.io/driver/sqlite v1.6.0
